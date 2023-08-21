fc8e509986f595dacccb9dfedd65967ab2d2e198
CosmosDever <aprommhgot@gmail.com>
2023-08-21 11:29:13 +0700

    .

---
+++ Comment_test/CommentsTest.java
@@ -0,0 +1,12 @@
+package java_study.Comment_test;
+//Create class CommentsTest
+public class CommentsTest {
+    public static void main(String[] args)
+    {
+        //This is a single-line comment
+        System.out.println("Hello World!");
+        /*This is multi-line */
+        System.out.println("Hello World!");
+    }
+    
+}
--- java_FE/FirstExample.java
+++ java_FE/FirstExample.java
@@ -7,8 +7,13 @@ public class FirstExample
     public static void main(String[] args) {
         JFrame myWindow;
         myWindow = new JFrame();
-        myWindow.setSize(300,200);
+        myWindow.setSize(600,600);
         myWindow.setTitle("My First Java Program");
+        JLabel label1=new JLabel("KO"); //Label is a component that displays text or images on the screen
+        label1.setHorizontalAlignment(JLabel.CENTER);
+        myWindow.add(label1);
         myWindow.setVisible(true);
     }
 }
+
+
