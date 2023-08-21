b470e36db9fd13af25a926aac381294e439b1b9a
CosmosDever <aprommhgot@gmail.com>
2023-08-21 11:38:34 +0700

    Create varTest.java

---
+++ VariableTest/varTest.java
@@ -0,0 +1,28 @@
+package java_study.VariableTest;
+
+public class varTest {
+    public static void main(String[] args)
+    {
+        //String
+        String name = "POOM";
+        //char
+        char Grade = 'A';
+        //int
+        int classroom = 706;
+        //froat
+        float score =99.99f;
+        //boolean
+        boolean checkStatus=true;
+
+        //print
+        System.out.println("name : "+name);
+        System.out.println("Grade : "+Grade);
+        System.out.println("classroom : "+classroom);
+        System.out.println("score : "+score);
+        System.out.println("Check : "+checkStatus);
+
+
+
+    }
+    
+}
