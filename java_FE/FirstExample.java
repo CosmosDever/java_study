b02bc3d948eacacd48a79a9b160a1a63cab1c803
CosmosDever <aprommhgot@gmail.com>
2023-08-21 10:52:37 +0700

    Create FirstExample.java

---
+++ java_FE/FirstExample.java
@@ -0,0 +1,14 @@
+package java_study.java_FE;
+
+import javax.swing.*;
+
+public class FirstExample
+{
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("My First Java Program");
+        myWindow.setVisible(true);
+    }
+}
